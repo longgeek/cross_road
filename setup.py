8648a6a9932146e8c21fc96a1b38fe8631f81b61
frank <frank@thstack.com>
2014-11-21 23:13:49 +0800

    add setup.py
    Change-Id: Ia1a42f06278c06e56ed5bd312193fef9f7093666

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='cross_road',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Frank',
+    author_email='frank@thstack.com',
+)
